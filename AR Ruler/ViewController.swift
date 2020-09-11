1bcb2fc84ee7052a71ee8d2b2255df78292eb010
AechGG <hazg@me.com>
2020-09-11 12:02:18 +0100

    feat: calc distance between two points in 3D space

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var dotNodes = [SCNNode]();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -56,7 +58,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         }
     }
     
-    func addDot(at hitResult: ARHitTestResult) {
+    private func addDot(at hitResult: ARHitTestResult) {
         let dotGeometry = SCNSphere(radius: 0.005);
         let material = SCNMaterial();
         material.diffuse.contents = UIColor.red;
@@ -67,6 +69,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         dotNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y, hitResult.worldTransform.columns.3.z);
         
         sceneView.scene.rootNode.addChildNode(dotNode);
+        
+        dotNodes.append(dotNode);
+        
+        if dotNodes.count >= 2 {
+            calculate();
+        }
+    }
+    
+    private func calculate() {
+        let start = dotNodes[0];
+        let end = dotNodes[1];
+        
+        let a = end.position.x - start.position.x;
+        let b = end.position.y - start.position.y;
+        let c = end.position.z - start.position.z;
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
+        
+        print(abs(distance));
     }
 
 }
